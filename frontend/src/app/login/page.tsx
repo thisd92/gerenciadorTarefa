642c9651dce27c0f6355748866eea75e4a5ee175
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-06-01 21:18:47 -0300

    alter User Login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,16 +1,33 @@
 'use client'
 import Link from "next/link";
 import { useState } from "react";
-import { User } from "./type";
+import { UserLogin } from "./type";
 import { BASE_URL } from "../../../utils/request";
 import axios from "axios";
 
 export default function Login() {
 
-    const [user, setUser] = useState<User>();
-    
-    function sendUser(){
-        axios.post(`${BASE_URL}/login`, user);
+    const newUserLogin = {
+        username: "",
+        password: ""
+    }
+
+    function handleUsername(e: any) {
+        const { name } = e.target;
+        setUserLogin({ ...userLogin, [name]: e.target.value })
+    }
+
+    function handlePassword(e: any) {
+        const { name } = e.target;
+        setUserLogin({ ...userLogin, [name]: e.target.value })
+    }
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(newUserLogin);
+
+    function sendUser(e: React.FormEvent) {
+        e.preventDefault()
+        axios.post(`${BASE_URL}/usersLogin`, userLogin);
+        console.log(userLogin)
     }
 
     return (
@@ -22,19 +39,23 @@ export default function Login() {
                         <form onSubmit={sendUser}>
                             <div className="mt-2">
                                 <label className="block text-gray-700 text-sm font-bold mb-2 " htmlFor="username">Username or e-mail</label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                                type="text" name="username" id="username" />
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    type="text" name="username" id="username" value={userLogin.username} onChange={handleUsername} />
                             </div>
                             <div className="mt-2">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                                type="password" name="password" id="password" />
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    type="password" name="password" id="password" value={userLogin.password} onChange={handlePassword} />
                             </div>
                             <div className="mt-2 flex items-center justify-between">
                                 <button className="bg-green-600 shadow-md rounded-md hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline" type="submit">Sign In</button>
                                 <Link className="inline-block align-baseline font-bold text-sm hover:text-green-700" href="#">
                                     Forgot Password?
                                 </Link>
+                            </div>
+                            <div className="mt-2 flex items-center justify-center">
+                                <p className="text-sm">Dont hava a account? <Link className="inline-block align-baseline font-bold text-sm hover:text-green-700" href="/register">Sign Up</Link>
+                                </p>
                             </div>
                         </form>
                     </div>
--- frontend/src/app/login/type.ts
+++ frontend/src/app/login/type.ts
@@ -1,4 +1,4 @@
-export type User = {
+export type UserLogin = {
     username: string,
     password: string
 }
