f1cb2febd1a86cedc698914765cb4b5c9cdcb511
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-06-03 05:52:05 -0300

    adjust payload

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -8,12 +8,14 @@ import axios from "axios";
 export default function Login() {
 
     const newUserLogin = {
-        username: "",
+        email: "",
         password: ""
     }
 
     const [userLogin, setUserLogin] = useState<UserLogin>(newUserLogin);
     const [errorMsg, setErrorMsg] = useState('');
+    const [isLoading, setIsLoading] = useState(false); // Estado para controlar o indicador de carregamento
+
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         const { name } = e.target;
@@ -23,14 +25,17 @@ export default function Login() {
     async function handleSubmit(e: React.FormEvent) {
         e.preventDefault()
         try {
+            setIsLoading(true); // Ativar o indicador de carregamento
             const response = await axios.post(`${BASE_URL}/api/usersLogin`, userLogin);
             if (response.status === 200) {
                 window.location.href = "/taskManager";
             }
         } catch (error) {
-            setErrorMsg("Invalid email or password")
+            setErrorMsg("Invalid Email or Password")
             setTimeout(() => setErrorMsg(""), 2000)
             console.log(error)
+        }finally{
+            setIsLoading(false);
         };
         console.log(userLogin)
     }
@@ -43,9 +48,9 @@ export default function Login() {
                     <div className="flex flex-col mt-4">
                         <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
                             <div>
-                                <label className="block text-gray-700 text-sm font-bold mb-2 " htmlFor="username">E-mail</label>
+                                <label className="block text-gray-700 text-sm font-bold mb-2 " htmlFor="email">E-mail</label>
                                 <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type="text" name="username" id="username" value={userLogin.username} onChange={handleChange} />
+                                    type="email" name="email" id="email" value={userLogin.email} onChange={handleChange} />
                             </div>
                             <div className="flex flex-col">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
@@ -54,7 +59,8 @@ export default function Login() {
                                 <span className="text-red-600">{errorMsg}</span>
                             </div>
                             <div className="flex items-center justify-between">
-                                <button className="bg-green-600 shadow-md rounded-md hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline" type="submit">Sign In</button>
+                                <button className="bg-green-600 shadow-md rounded-md hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline" 
+                                type="submit" disabled={isLoading} >{isLoading ? "Signing In" : "Sign In" }</button>
                                 <Link className="inline-block align-baseline font-bold text-sm hover:text-green-700" href="#">
                                     Forgot Password?
                                 </Link>
