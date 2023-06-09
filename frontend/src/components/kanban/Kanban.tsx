ecbb5c7b3c4ea2bdda98862de4697fbbbae6b900
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-06-08 23:29:10 -0300

    Fix Kanban Component

--- frontend/src/components/kanban/Kanban.tsx
+++ frontend/src/components/kanban/Kanban.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
-import { Task } from '@/app/taskManager/type';
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8090';
+import { Task } from '@/app/taskManager/type';
+import { BASE_URL } from '../../../utils/request';
 
 interface KanbanProps {
     tasks: Task[];
+    getTasks: () => void;
 }
 
-const Kanban: React.FC<KanbanProps> = ({ tasks }) => {
+const Kanban: React.FC<KanbanProps> = ({ tasks, getTasks }) => {
     const [taskList, setTaskList] = useState<Task[]>(tasks);
 
     useEffect(() => {
-        setTaskList(tasks);
+        getTasks(); // Atualiza o quadro quando o componente é renderizado
+    }, []);
+    
+    useEffect(() => {
+        setTaskList(tasks); // Atualiza o quadro quando houver alteração nas tarefas
     }, [tasks]);
 
     const handleDragEnd = async (result: DropResult) => {
@@ -26,14 +31,20 @@ const Kanban: React.FC<KanbanProps> = ({ tasks }) => {
         }
 
         const updatedTasks = Array.from(taskList);
-        const movedTask = updatedTasks[source.index];
-        updatedTasks.splice(source.index, 1);
+        const movedTask = updatedTasks.splice(source.index, 1)[0];
         updatedTasks.splice(destination.index, 0, movedTask);
+
+        const taskIdDrag = result.draggableId;
+        const taskMoved = await (await axios.get(`${BASE_URL}/api/tasks/${taskIdDrag}`)).data;
 
         setTaskList(updatedTasks);
 
         try {
-            const { _id } = movedTask;
+            const taskId = movedTask._id || movedTask._id;
+            if (!taskId) {
+                console.error('Task ID not found.');
+                return;
+            }
             let newStatus: string;
 
             if (destination.droppableId === 'toDo') {
@@ -47,125 +58,80 @@ const Kanban: React.FC<KanbanProps> = ({ tasks }) => {
             }
 
             const updatedTaskData = {
-                name: movedTask.name,
-                description: movedTask.description,
+                name: taskMoved.name,
+                description: taskMoved.description,
                 toDo: newStatus === 'toDo',
                 isInProgress: newStatus === 'inProgress',
                 isFinished: newStatus === 'finished',
             };
 
-            await axios.put(`${BASE_URL}/api/tasks/${_id}`, updatedTaskData);
+            await axios.put(`${BASE_URL}/api/tasks/${taskIdDrag}`, updatedTaskData);
+            getTasks();
             console.log('Task updated successfully');
         } catch (error) {
             console.error('Error updating task:', error);
         }
     };
 
-    const handleStatusChange = async (taskId: string, newStatus: string) => {
-        try {
-            const updatedTasks = taskList.map((task) => {
-                if (task._id === taskId) {
-                    switch (newStatus) {
-                        case 'toDo':
-                            return { ...task, toDo: true, isInProgress: false, isFinished: false };
-                        case 'inProgress':
-                            return { ...task, toDo: false, isInProgress: true, isFinished: false };
-                        case 'finished':
-                            return { ...task, toDo: false, isInProgress: false, isFinished: true };
-                        default:
-                            return task;
-                    }
-                }
-                return task;
-            });
-
-            setTaskList(updatedTasks);
-
-            await axios.put(`${BASE_URL}/api/tasks/${taskId}`, { status: newStatus });
-            console.log('Task updated successfully');
-        } catch (error) {
-            console.error('Error updating task:', error);
-        }
-    };
+    const renderTaskItems = (taskList: Task[], className: string) =>
+        taskList.map((task, index) => (
+            <Draggable key={task._id} draggableId={task._id ?? ''} index={index} data-task-id={task._id}>
+                {(provided) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className={className}
+                    >
+                        <p>{task.name}</p>
+                    </div>
+                )}
+            </Draggable>
+        ));
 
     return (
         <section className="w-2/4 border-2 border-slate-300 rounded-md shadow-lg px-8 pt-6 pb-8 mb-4">
             <DragDropContext onDragEnd={handleDragEnd}>
-                <div className="flex flex-row gap-4">
-                    <Droppable droppableId="toDo">
-                        {(provided) => (
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-xl text-center font-bold mb-2">To Do</h2>
-                                <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
-                                    {taskList.filter((task) => task.toDo).map((task, index) => (
-                                        <Draggable key={task._id} draggableId={task._id ?? ""} index={index}>
-                                            {(provided) => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    className="bg-gray-200 p-4 rounded-md"
-                                                >
-                                                    <p>{task.name}</p>
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
+                <div className="flex flex-row gap-4 justify-around">
+                    <div>
+                        <Droppable droppableId="toDo">
+                            {(provided) => (
+                                <div className="flex flex-col gap-2">
+                                    <h2 className="text-xl text-center font-bold mb-2">To Do</h2>
+                                    <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
+                                        {renderTaskItems(taskList.filter((task) => task.toDo), 'bg-blue-200 p-4 rounded-md')}
+                                        {provided.placeholder}
+                                    </div>
                                 </div>
-                            </div>
-                        )}
-                    </Droppable>
-
-                    <Droppable droppableId="inProgress">
-                        {(provided) => (
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-xl text-center font-bold mb-2">In Progress</h2>
-                                <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
-                                    {taskList.filter((task) => task.isInProgress).map((task, index) => (
-                                        <Draggable key={task._id} draggableId={task._id ?? ""} index={index}>
-                                            {(provided) => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    className="bg-yellow-200 p-4 rounded-md"
-                                                >
-                                                    <p>{task.name}</p>
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
+                            )}
+                        </Droppable>
+                    </div>
+                    <div>
+                        <Droppable droppableId="inProgress">
+                            {(provided) => (
+                                <div className="flex flex-col gap-2">
+                                    <h2 className="text-xl text-center font-bold mb-2">In Progress</h2>
+                                    <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
+                                        {renderTaskItems(taskList.filter((task) => task.isInProgress), 'bg-yellow-200 p-4 rounded-md')}
+                                        {provided.placeholder}
+                                    </div>
                                 </div>
-                            </div>
-                        )}
-                    </Droppable>
-
-                    <Droppable droppableId="finished">
-                        {(provided) => (
-                            <div className="flex flex-col gap-2">
-                                <h2 className="text-xl text-center font-bold mb-2">Finished</h2>
-                                <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
-                                    {taskList.filter((task) => task.isFinished).map((task, index) => (
-                                        <Draggable key={task._id} draggableId={task._id ?? ''} index={index}>
-                                            {(provided) => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    className="bg-green-200 p-4 rounded-md"
-                                                >
-                                                    <p>{task.name}</p>
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
+                            )}
+                        </Droppable>
+                    </div>
+                    <div>
+                        <Droppable droppableId="finished">
+                            {(provided) => (
+                                <div className="flex flex-col gap-2">
+                                    <h2 className="text-xl text-center font-bold mb-2">Finished</h2>
+                                    <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2">
+                                        {renderTaskItems(taskList.filter((task) => task.isFinished), 'bg-green-200 p-4 rounded-md')}
+                                        {provided.placeholder}
+                                    </div>
                                 </div>
-                            </div>
-                        )}
-                    </Droppable>
+                            )}
+                        </Droppable>
+                    </div>
                 </div>
             </DragDropContext>
         </section>
